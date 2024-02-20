3d82e1ad046b0c110c52ff29a4e8f332c412eac3
LinCanhui <lingyun2311@gmail.com>
2024-02-20 22:26:31 +0800

    使用 Breadcrumb 实现面包屑导航

---
+++ src/views/Home/components/BreadCrumb.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="breadcrumb">
+    <el-breadcrumb class="bread-crumb" separator="/">
+      <el-breadcrumb-item :to="{ path: '/' }">全部</el-breadcrumb-item>
+      <el-breadcrumb-item>文件夹1</el-breadcrumb-item>
+      <el-breadcrumb-item>文件夹1-1</el-breadcrumb-item>
+    </el-breadcrumb>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BreadCrumb",
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped lang="stylus">
+.bread-crumb {
+  height: 28px;
+  line-height: 28px;
+}
+</style>
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,16 +1,24 @@
 <template>
-  <div class="home">
-    <!--使用左侧菜单组件-->
-    <SideMenu></SideMenu>
+  <div id="home">
+    <!-- 左侧菜单 - 区分文件类型 -->
+    <SideMenu class="home-left"></SideMenu>
+    <!-- 右侧内容区 -->
+    <div class="home-right">
+      <!-- 面包屑导航栏 - 显示文件路径 -->
+      <BreadCrumb></BreadCrumb>
+    </div>
   </div>
 </template>
 
 <script>
 import SideMenu from "./components/SideMenu.vue"; // 引入左侧菜单组件
+import BreadCrumb from "./components/BreadCrumb.vue"; // 面包屑导航
+
 export default {
   name: "HomeView",
   components: {
     SideMenu, // 注册左侧菜单组件
+    BreadCrumb,
   },
   data() {
     return {};
@@ -18,3 +26,21 @@ export default {
   methods: {},
 };
 </script>
+
+<style lang="stylus" scoped>
+#home {
+  // 使用flex布局 菜单居左固定宽度 右侧内容区域自适应宽度
+  display: flex;
+
+  .home-left {
+    box-sizing: border-box;
+  }
+
+  .home-right {
+    box-sizing: border-box;
+    width: calc(100% - 200px);
+    padding: 8px 24px;
+    flex: 1;
+  }
+}
+</style>
